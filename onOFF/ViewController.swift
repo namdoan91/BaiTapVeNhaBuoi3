a7eead6d369fb5515e978753f200e8b0539898ae
NAMIT <dnhnam91@gmail.com>
2020-10-26 19:17:00 +0700

    baitapvenhabuoi3

--- onOFF/ViewController.swift
+++ onOFF/ViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var iMage: UIImageView!
+    @IBOutlet weak var onOFF: UISwitch!
+    @IBOutlet weak var batTat: UIButton!
+    
+    var isON : Bool!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        onOFF.isOn = true
+        isON = true
+        iMage.image = UIImage(named: "bulb-on")
+        onOFF.thumbTintColor = UIColor.gray
+        
+        batTat.frame = CGRect(x:0, y: 780, width: 394, height: 30)
+        batTat.setTitle("Bật đèn", for: .normal)
     }
 
-
+    @IBAction func offImage(_ sender: UISwitch) {
+        BatTatDen();
+    }
+    @IBAction func onPress(_ sender: Any) {
+        BatTatDen();
+    }
+    
+    func BatTatDen(){
+        if isON{
+            batTat.setTitle("Bật đèn", for: .normal)
+            iMage.image = UIImage(named:"bulb-off")
+            onOFF.isOn = false
+//            isON = false
+        }else{
+            batTat.setTitle("Tắt đèn", for: .normal)
+            iMage.image = UIImage(named:"bulb-on")
+            onOFF.isOn = true
+//            isON = false
+        }
+        isON = !isON
+    }
+    
 }
 
